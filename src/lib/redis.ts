4e0f36b55dd3e650e1e5e75c05525d5eb0a19eff
rajpragur <rajpragur@gmail.com>
2026-02-20 20:00:20 +0530

    updated redis config

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,14 +1,15 @@
 import { Redis } from '@upstash/redis'
 
 // Check if credentials exist to avoid crashing the app if they are missing
-const hasRedisCredentials =
-    !!process.env.UPSTASH_REDIS_REST_URL &&
-    !!process.env.UPSTASH_REDIS_REST_TOKEN
+const REDIS_URL = process.env.UPSTASH_REDIS_REST_URL || process.env.KV_REST_API_URL
+const REDIS_TOKEN = process.env.UPSTASH_REDIS_REST_TOKEN || process.env.KV_REST_API_TOKEN
+
+const hasRedisCredentials = !!REDIS_URL && !!REDIS_TOKEN
 
 // Create a single global instance for the application
 export const redis = hasRedisCredentials
     ? new Redis({
-        url: process.env.UPSTASH_REDIS_REST_URL || '',
-        token: process.env.UPSTASH_REDIS_REST_TOKEN || '',
+        url: REDIS_URL || '',
+        token: REDIS_TOKEN || '',
     })
     : null
